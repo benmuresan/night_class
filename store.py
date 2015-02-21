0f266abb63fe0b29f73d66fa51ad49548e22d500
Beniamin Muresan <Beni@Benis-MacBook.local>
2015-02-20 20:55:39 -0800

    store update

--- store.py
+++ store.py
@@ -15,21 +15,33 @@ class Customer(object):
     def __init__(self):
         self.name = ""
         self.balance = 0
-        self.cart = []
+        self.cart = {}
 
-    def buy(self, item):
-        self.balance = self.balance - item.get("price")
-        self.cart.append(item)
-        self.balance = self.balance - item.get("price")
-        self.pay = Store.money + item.get("price")
+    def buy(self, store, item, quantity):
+        self.balance -= (store.inventory[item]["price"] * quantity)
+        self.cart[item] = quantity
+        store.money += (store.inventory[item]["price"] * quantity)
+        store.inventory[item]["quantity"] -= quantity
 
-bens_store = Store()
+
+
+kens_store = Store()
+nathans_store = Store()
 
 cust1 = Customer()
+cust2 = Customer()
 
-cust1.buy("bread")
+cust1.buy(kens_store, "Bread", 2)
+cust2.buy(nathans_store, "Milk", 2)
 
-if "Bread" in bens_store.inventory:
-    item = bens_store.inventory.get("Bread")
+print str(kens_store.money) + "\n"
+print str(kens_store.inventory) + "\n"
+print str(nathans_store.money) + "\n"
+print str(nathans_store.inventory) + "\n"
+print(cust1.cart)
+print(cust1.balance)
+
+# if "Bread" in bens_store.inventory:
+#     item = bens_store.inventory.get("Bread")
 
 
